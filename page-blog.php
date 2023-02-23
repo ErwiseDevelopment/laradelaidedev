2b8b468e39aa7384593c404680bbd49291246b92
ErwiseDevelopment <erwise.dev@gmail.com>
2023-02-23 12:34:31 -0300

    lar adelaide noticias

--- page-blog.php
+++ page-blog.php
@@ -21,7 +21,7 @@ get_header(); ?>
  $id_url = $_GET['id'];
 
  if(empty($_GET['id'])) {
-    header('Location: https://lar.erwisedev-hml.com.br/blogs');
+    echo "<script>window.location.href='https://lar.erwisedev-hml.com.br/blogs';</script>";
     exit();
 }
 
